b7d33980dafbe528eb73d912fcd56e78873d9deb
Alexey Zapadnov <saings@yandex.ru>
2018-12-22 11:57:11 +0300

    done

---
+++ hw15/src/ISearchEngine.java
@@ -0,0 +1,11 @@
+import java.util.List;
+//
+//
+//
+//
+//
+//
+//
+public interface ISearchEngine {
+    List<ITextProvider> getSortedByRelevanceList(ITextAnalyze textAnalyze, ITextProvider query, List<ITextProvider> sources);
+}
---
+++ hw15/src/TitleTextProviderImpl.java
@@ -0,0 +1,18 @@
+import java.io.File;
+//
+//
+//
+//
+//
+//
+//
+public class TitleTextProviderImpl extends FileTextProviderImpl {
+    public TitleTextProviderImpl(File input) {
+        super(input);
+    }
+
+    @Override
+    public String getText() {
+        return input.getName() + " " + super.getText();
+    }
+}
